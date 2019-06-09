d927133237cf9297e04fffbca28b830ac9148902
juulyy <bienvenu.julia@gmail.com>
2019-06-09 22:44:02 +0300

    Refactor

--- source.cpp
+++ source.cpp
@@ -2,6 +2,8 @@
 #include <ctime>
 #include <math.h>
 
+#include "TextTable.h"
+
 using namespace std;
 
 struct function_parametrs {
@@ -12,23 +14,15 @@ struct function_parametrs {
     float dx = 0.5;
 } params;
 
-char *DrawLine(int N);
-
-float function(function_parametrs params, float x);
-
-void Print_row(float x, int N, float y);
-
 void Tab();
-
-void RandomArray();
-
-void FxToDecimal();
-
 void Print();
+float function(function_parametrs params, float x);
+void random_array();
+void fx_to_integer();
 
 const int A = 65;
 const float delta = 0.035f;
-int *fx_decimal;
+int *fx_integer;
 float *rnd;
 float *fx;
 float Smin, Smax;
@@ -36,91 +30,99 @@ int N;
 
 
 int main() {
+
     Tab();
     cout << endl;
-//    system("pause");
-    RandomArray();
+    random_array();
     cout << endl;
-//    system("pause");
     Print();
-
     cout << endl;
-//    system("pause");
+
     return 0;
 }
 
 void Print() {
-    FxToDecimal();
+    fx_to_integer();
     cout << endl;
     for (int i = 0; i < N; i++) {
-        for (int j = 0; j < fx_decimal[i]; j++)
-            cout << char(0xDB);
+        for (int j = 0; j < fx_integer[i]; j++) {
+            cout << "\u0333";
+        }
         cout << endl;
     }
     cout << endl;
 }
 
 
-void FxToDecimal() {
-    fx_decimal = new int[N];
+void fx_to_integer() {
+    fx_integer = new int[N];
     for (int i = 0; i < N; i++) {
         int y = round(fx[i]);
         if (y < 0)
             y = 0;
         else if (y > 65)
             y = 65;
-        fx_decimal[i] = y;
+        fx_integer[i] = y;
     }
 }
 
-void RandomArray() {
+void random_array() {
     printf("\nSmax = %.3f\tSmin = %.3f\n", Smax, Smin);
     srand((unsigned) time(0));
     rnd = new float[N];
     float step = Smax * delta;
     int n = (int) ((Smax - Smin) / step);
-    printf("\n%c%.7s%c%.12s%c", 218, DrawLine(7), 194, DrawLine(12), 191);
-    printf("\n%c %-5s %c %-10s %c", 179, "N", 179, "f(x)", 179);
+
+    TextTable textTable( '-', '|', '+' );
+    textTable.add( "N" );
+    textTable.add( "f(x)" );
+    textTable.endOfRow();
 
     for (int i = 0; i < N; i++) {
-        printf("\n%c%.7s%c%.12s%c", 195, DrawLine(7), 197, DrawLine(12), 180);
         rnd[i] = ((rand() % (n + 1)) * step) + Smin;
-        printf("\n%c %5d %c %10.5f %c", 179, i + 1, 179, rnd[i], 179);
+        textTable.add(to_string(i + 1));
+        textTable.add(to_string(rnd[i]));
+        textTable.endOfRow();
     }
-
-    printf("\n%c%.7s%c%.12s%c", 192, DrawLine(7), 193, DrawLine(12), 217);
-}
-
-char *DrawLine(int N) {
-    char *line = new char[N];
-    for (int i = 0; i < N; i++)
-        line[i] = char(196);
-    return line;
+    textTable.setAlignment(2, TextTable::Alignment::RIGHT);
+    std::cout << textTable;
 }
 
 void Tab() {
     N = (int) ((params.x2 - params.x1) / params.dx) + 1;
     fx = new float[N];
 
-    printf("\n%c%.7s%c%.7s%c%.12s%c", 218, DrawLine(7), 194, DrawLine(7), 194, DrawLine(12), 191);
-    printf("\n%c %-5s %c %-5s %c %-10s %c", 179, "x", 179, "N", 179, "f(x)", 179);
     float x = params.x1;
     float S1, S2;
     S1 = S2 = 0;
 
+    TextTable textTable('-', '|', '+');
+    textTable.add("x");
+    textTable.add("N");
+    textTable.add("f(x)");
+    textTable.endOfRow();
+
     for (int i = 0; i < N; i++) {
-        printf("\n%c%.7s%c%.7s%c%.12s%c", 195, DrawLine(7), 197, DrawLine(7), 197, DrawLine(12), 180);
+
         float y = function(params, x);
         fx[i] = y;
-        Print_row(x, i + 1, y);
+        textTable.add(to_string(x));
+        textTable.add(to_string(i+1));
+        textTable.add(to_string(y));
+        textTable.endOfRow();
+
         x += params.dx;
-        if (y > 0)
+        if (y > 0) {
             S1 += y;
-        if ((i + 1) % 2 == 1)
+        }
+        if ((i + 1) % 2 == 1) {
             S2 += y;
+        }
     }
 
-    printf("\n%c%.7s%c%.7s%c%.12s%c\n", 192, DrawLine(7), 193, DrawLine(7), 193, DrawLine(12), 217);
+    textTable.setAlignment(2, TextTable::Alignment::RIGHT);
+    std::cout << textTable;
+
     if (S1 > S2) {
         Smax = S1;
         Smin = S2;
@@ -128,10 +130,6 @@ void Tab() {
         Smin = S1;
         Smax = S2;
     }
-}
-
-void Print_row(float x, int N, float y) {
-    printf("\n%c %5.1f %c %5d %c %10.5f %c", 179, x, 179, N, 179, y, 179);
 }
 
 float function(function_parametrs params, float x) {
